ce7ae21123afd846bba74eb5350618f5ee3e9cc9
AYYANAR PRASANNA B <144008049+Prasanna1204@users.noreply.github.com>
2024-02-22 18:16:29 +0530

    final code-part-4.py

--- caesar-cipher/part-4.py
+++ caesar-cipher/part-4.py
@@ -1,10 +1,7 @@
+# here is the full code of caesar cipher project using functions and conditional loops
 from art import logo
 print(logo)
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']','/','|',';',':',',','.','/','<','>','!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']','/','|',';',':',',','.','/','<','>']
 def ceaser(direction_ips,word_text,shift_amount):
     ceaser_cipher = ""
     if direction_ips == "decode":
@@ -26,4 +23,4 @@ while not should_end:
   restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
   if restart == "no":
     should_end = True
-    print("Goodbye")
+    print("Goodbye!")
